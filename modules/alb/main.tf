44d0b6b647a0c7fa36c4e5f65172decd8e015e7f
Erik Andreazi <andreazi.erik@gmail.com>
2024-04-23 22:36:26 -0300

    Iniciando modulo para provisionamento do ALB, TG e outros serviços necessarios

---
+++ modules/alb/main.tf
@@ -0,0 +1,61 @@
+resource "aws_security_group" "alb-sg" {
+  name   = "${var.projeto}-alb-sg"
+  vpc_id = var.vpcid
+
+  tags = {
+    Name = "${var.projeto}-alb-sg"
+  }
+}
+
+resource "aws_security_group_rule" "alb-sg-ingress" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.alb-sg.id
+  cidr_blocks       = var.any-ip
+}
+
+resource "aws_lb_target_group" "projeto17-tg" {
+  name     = "${var.projeto}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpcid
+
+
+  health_check {
+    enabled             = true
+    port                = 80
+    interval            = 50
+    protocol            = "HTTP"
+    path                = "/"
+    matcher             = "200-299"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+}
+
+resource "aws_lb" "projeto17-alb" {
+  name               = "${var.projeto}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb-sg.id]
+
+  subnets = var.public-subnets-ids
+
+  access_logs {
+    bucket  = var.bucket-name
+    enabled = true
+  }
+}
+
+resource "aws_lb_listener" "name" {
+  load_balancer_arn = aws_lb.projeto17-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.projeto17-tg.arn
+  }
+}
---
+++ modules/alb/variables.tf
@@ -0,0 +1,24 @@
+variable "projeto" {
+  type        = string
+  description = "Nome do projeto"
+}
+
+variable "vpcid" {
+  type        = string
+  description = "ID da VPC"
+}
+
+variable "any-ip" {
+  type        = list(string)
+  description = "IP corringa"
+}
+
+variable "public-subnets-ids" {
+  type        = list(string)
+  description = "IDs das subnets publicas para associar ao ALB"
+}
+
+variable "bucket-name" {
+  type        = string
+  description = "Bucket para armazenar os logs"
+}
